5c667101755cd70363552eec12678968d3b84512
seun olota <155106182+seuneo@users.noreply.github.com>
2024-02-29 09:41:17 -0500

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -2,9 +2,9 @@ var resume = document.querySelector(".grad-header");
 
 resume.onmouseenter = function(){
 
-    resume.innerHTML = "Download resume"; 
+    resume.innerHTML = "Download resume<span class=\"material-symbols-outlined\"> file_save</span>"; 
 };
 
 resume.onmouseout = function(){
-    resume.innerHTML = "Since Graduating"; 
+    resume.innerHTML = "Since Graduating <span class=\"material-symbols-outlined\">file_save</span>"; 
 };
